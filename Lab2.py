32e60beb49805bcec822cf339b7fbba160de715b
moe453 <107380496+moe453@users.noreply.github.com>
2022-06-13 00:19:22 -0400

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+def function ():
+    arguments = sys.argv
+    name = str(sys.argv[1])
+    age = int(sys.argv[2])
+
+    print('Variable1 = ' + sys.argv[1])
+    print('Variable2 = ' + str(age))
+    print('The script and Variables used are:' + sys.argv)
+    
